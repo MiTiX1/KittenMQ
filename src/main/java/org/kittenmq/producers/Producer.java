a28a285ec8cbfbe93ac32ebfe3766c19b008f1c2
MiTiX <bast.boulet@gmail.com>
2024-10-20 16:43:49 +0200

    feat: add headers to message

--- src/main/java/org/kittenmq/producers/Producer.java
+++ src/main/java/org/kittenmq/producers/Producer.java
@@ -4,6 +4,8 @@ import org.kittenmq.brokers.Broker;
 import org.kittenmq.messages.Message;
 import org.kittenmq.messages.MessageQueue;
 
+import java.util.Map;
+
 public class Producer<T> {
     private final String name;
     private final Broker broker;
@@ -21,6 +23,15 @@ public class Producer<T> {
             throw new IllegalArgumentException("Queue does not exist: " + queueName);
         }
         Message<T> message = new Message<T>(payload);
+        queue.enqueue(message);
+    }
+
+    public void sendMessage(T payload, Map<String, String> headers) throws InterruptedException {
+        MessageQueue<Message<?>> queue = broker.getQueue(queueName);
+        if (queue == null) {
+            throw new IllegalArgumentException("Queue does not exist: " + queueName);
+        }
+        Message<T> message = new Message<T>(payload, headers);
         queue.enqueue(message);
     }
 
