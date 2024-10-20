ad477d4616e1e0e99ced9360712b3f388c43eb08
MiTiX <bast.boulet@gmail.com>
2024-10-20 15:05:33 +0200

    feat: added dead letter queue

--- src/main/java/org/kittenmq/messages/MessageQueue.java
+++ src/main/java/org/kittenmq/messages/MessageQueue.java
@@ -2,18 +2,15 @@ package org.kittenmq.messages;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class MessageQueue<T> {
-    private final BlockingQueue<T> queue;
+    private final BlockingQueue<T> queue = new LinkedBlockingQueue<T>();;
+    private final MessageQueue<T> deadLetterQueue;
     private final String name;
 
-    public MessageQueue(String name) {
-        this.queue = new LinkedBlockingQueue<T>();
-        this.name = name;
-    }
-
-    public MessageQueue(String name, int capacity) {
-        this.queue = new LinkedBlockingQueue<>(capacity);
+    public MessageQueue(String name, MessageQueue<T> deadLetterQueue) {
+        this.deadLetterQueue = deadLetterQueue;
         this.name = name;
     }
 
@@ -23,6 +20,20 @@ public class MessageQueue<T> {
 
     public T dequeue() throws InterruptedException {
         return this.queue.take();
+    }
+
+    public T dequeue(long timeout, TimeUnit unit) throws InterruptedException {
+        return this.queue.poll(timeout, unit);
+    }
+
+    public void moveToDeadLetterQueue(T message) throws InterruptedException {
+        if (this.deadLetterQueue != null) {
+            this.deadLetterQueue.enqueue(message);
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.queue.isEmpty();
     }
 
     public String getName() {
